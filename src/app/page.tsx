f143eb835f68eadb920d12042206212e78f22b88
Teena_Jadhao <teenajadhao@gmail.com>
2026-02-15 21:09:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <About />
       <Education />
       <Experience />
-      <Projects />
+  
       <Skills />
       <Leadership />
       <Certifications />
